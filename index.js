70bac30ecae4d9d97ed3138c733550e5ec2eca7f
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2019-03-30 03:03:04 -0300

    server initialization with config.js and index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config(); // setup dotenv as soon as the app starts
+
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+const environment = process.env.NODE_ENV // DEV
+const stage = require('./config')[environment];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+if (environment !== 'production') {
+    app.use(logger('dev'));
+}
+
+app.use('/api/v1', (req, res, next) => {
+    res.send('Hello');
+    next();
+});
+
+app.listen(`${stage.port}`, () => {
+    console.log(`Server listening at localhost:${stage.port}`);
+});
+
+module.exports = app;
